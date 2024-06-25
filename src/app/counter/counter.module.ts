df249f03f2a6526caee2a29881c6b3c1621416df
Miguel Angel Soto Cruz <miguel.soto@autoridadminera.gob.bo>
2024-06-25 16:53:08 -0400

    inicial

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { CounterComponent } from "./components/counter/counter.component";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [
+    CounterComponent
+  ],
+  exports: [
+    CounterComponent
+  ]
+})
+export class CounterModule {
+
+}
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/Character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string):void {
+    // TODO: Emitir el ID del personaje
+    // console.log({id});
+    if (!id) return;
+    this.onDelete.emit(id)
+  }
+}
